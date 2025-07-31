608c59b22d15586f5dc25bebb0463b0aea65eeaf
DiilaNa <liyanaarachchidilan@gmail.com>
2025-07-31 14:26:15 +0530

    add service

---
+++ BackEnd/src/main/java/lk/ijse/project/backend/service/AuthService.java
@@ -0,0 +1,50 @@
+package lk.ijse.project.backend.service;
+
+import lk.ijse.project.backend.dto.AuthDto;
+import lk.ijse.project.backend.dto.AuthResponseDto;
+import lk.ijse.project.backend.dto.RegisterDto;
+import lk.ijse.project.backend.entity.User;
+import lk.ijse.project.backend.repository.UserRepository;
+import lk.ijse.project.backend.util.JWTUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtil jwtUtil;
+
+    public AuthResponseDto authenticate(AuthDto authDto) {
+
+
+       User user =  userRepository.findByUserame(authDto.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+       if (!passwordEncoder.matches(
+               authDto.getPassword(),
+               user.getPassword()))
+       {
+           throw new BadCredentialsException("Bad credentials");
+       }
+       //generate token
+       String token = jwtUtil.generateToken(authDto.getUsername());
+        return new AuthResponseDto(token);
+    }
+
+    public String Register(RegisterDto registerDto) {
+        if (userRepository.findByUserame(registerDto.getUsername()).isPresent()){
+            throw new RuntimeException("Username already exists");
+        }
+
+        User user = User.builder()
+                .userName(registerDto.getUsername())
+                .password(passwordEncoder.encode(registerDto.getPassword()))
+                .build();
+        userRepository.save(user);
+        return "User Register SuccessFully";
+    }
+}
