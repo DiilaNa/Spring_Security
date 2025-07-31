86a54ac4b1306475bf968abaafce07060f807114
DiilaNa <liyanaarachchidilan@gmail.com>
2025-07-31 11:46:10 +0530

    add Api AuthDto

---
+++ BackEnd/src/main/java/lk/ijse/project/backend/dto/AuthDto.java
@@ -0,0 +1,9 @@
+package lk.ijse.project.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthDto {
+    public String username;
+    public String password;
+}
---
+++ BackEnd/src/main/java/lk/ijse/project/backend/dto/RegisterDto.java
@@ -0,0 +1,10 @@
+package lk.ijse.project.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+    private String username;
+    private String password;
+    private String role;
+}
